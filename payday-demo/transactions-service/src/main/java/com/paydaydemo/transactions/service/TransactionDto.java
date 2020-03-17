8a332ea099821ab6edbe2bda80df1c39531eba82
Shakir Asadov <shakirasadov@gmail.com>
2020-03-17 14:13:18 +0400

    feature 3

--- payday-demo/transactions-service/src/main/java/com/paydaydemo/transactions/service/TransactionDto.java
+++ payday-demo/transactions-service/src/main/java/com/paydaydemo/transactions/service/TransactionDto.java
@@ -12,6 +12,7 @@ public class TransactionDto {
 	public String description;
 	public Date date;
 	public String feature1;
+	public String feature3;
     
 	public TransactionDto(Transaction transaction) {
 		super();
